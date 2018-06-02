7f35c9e521bc0b5aca4be69769b034bccd11c63b
cjh <1418503647@qq.com>
2018-06-02 19:08:00 +0800

    优化

--- src/main.js
+++ src/main.js
@@ -2,20 +2,15 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import http from '@/util/http'
-import qs from 'qs'
 import storage from '@/util/storage'
-import YunserUI from 'yunser-ui-vue'
 import ui from './components/index'
-import 'yunser-ui-vue/dist/yunser-ui.css'
 import './scss/main.scss'
 
 Vue.config.productionTip = false
 
 Vue.prototype.$http = http
-Vue.prototype.$qs = qs
 Vue.prototype.$storage = storage
 
-Vue.use(YunserUI)
 Vue.use(ui)
 
 /* eslint-disable no-new */
